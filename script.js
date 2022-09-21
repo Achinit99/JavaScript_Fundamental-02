926157acb0366a66c45301c6d169bb3ec96b86cc
Achinit99 <achinit720@gmail.com>
2022-09-21 21:32:55 +0530

    Activating String Mode

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+// Activating String Mode
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log ("I can drive :D");

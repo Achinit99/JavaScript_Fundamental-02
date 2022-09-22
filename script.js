1f8dbe376d455d6eac9e7a7e57f335818d3c6ce2
Achinit99 <achinit720@gmail.com>
2022-09-23 01:14:19 +0530

    Arrow Functions

--- script.js
+++ script.js
@@ -51,3 +51,16 @@ console.log (age1, age2);
 */
 
 // Arrow Functions
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3 (1999);
+console.log (age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    //return retirement;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log (yearsUntilRetirement (1999, `Achini`));
+console.log (yearsUntilRetirement (1997, `Avishka`));

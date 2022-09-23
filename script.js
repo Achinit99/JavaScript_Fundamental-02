5c73f3b78867eb1150baf8fddc2ebc4cd393e673
Achinit99 <achinit720@gmail.com>
2022-09-23 21:30:08 +0530

    Coding Challenge #1

--- script.js
+++ script.js
@@ -105,3 +105,23 @@ console.log (yearsUntilRetirement (1960, "Mike"));
 */
 
 // Coding Challenge #1
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log (calcAverage (3, 4, 5));
+
+// Test
+const scoreDolphins = calcAverage (44, 23, 71);
+const scoreKoalas = calcAverage (65, 54, 49);
+console.log (scoreDolphins, scoreKoalas);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+    if (avgDolphins >= 2 * avgKoalas) {
+        console.log (`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (avgKoalas >= 2 * avgDolphins) {
+        console.log (`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+    } else {
+        console.log ("No team wins...");
+    }
+}
+checkWinner (scoreDolphins, scoreKoalas);
+
+checkWinner (576, 111);

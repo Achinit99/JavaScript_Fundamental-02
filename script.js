3159ca52e08e99ce0537de76fe86d1d06dc42882
Achinit99 <achinit720@gmail.com>
2022-10-04 10:30:27 +0530

    Object Methods - Challenge

--- script.js
+++ script.js
@@ -280,10 +280,10 @@ console.log (`${jonas.firstName} has ${jonas.friends.length} friends, and his be
 const jonas = {
     firstName : 'Jonas',
     lastName : 'Schmedtmann',
-    age : 1991,
+    birthYear : 1991,
     job : 'teacher',
     friends : ['Michael', 'Achini', 'Kawshalya'],
-    hasDriversLicense : true,
+    hasDriversLicense : true, // false
 
     // calcAge : function (birthYear) {
     //     return 2037 - birthYear;
@@ -297,6 +297,10 @@ const jonas = {
     calcAge : function () {
             this.age = 2037 - this.birthYear;
             return this.age;
+    },
+
+    getSummary : function () {
+        return `${this.firstName} is a ${this.calcAge()}- year old ${jonas.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`;
     }
 
 };
@@ -305,3 +309,6 @@ console.log (jonas.calcAge ());
 //console.log (jonas.age ());
 // console.log (jonas ['calcAge'] (1991));
 
+// Challenge 
+// "Jonas is a 46-year old teacher, and he has a driver's license"
+console.log (jonas.getSummary ());
